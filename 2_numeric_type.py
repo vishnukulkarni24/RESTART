f33da40d683157bbfe47d733f5a9af4ecab0676c
vishnukulkarni24 <vishnukulkarni2426@gmail.com>
2025-10-31 13:42:24 +0530

    first commit data type

---
+++ 2_numeric_type.py
@@ -0,0 +1,24 @@
+## 1. 
+#a = 12
+#print('1. type--->',type(a))
+#b = 32
+#c = a + b
+#print(c,"<--type of output-->", type(c))
+
+'''
+d = 1.2
+e = 67
+f = e // d
+
+print(f"The OP ---> {f}\ntype of variable d & e ---> {type(d)} & {type(e)}")
+'''
+
+g = 10
+h = 20j
+
+i = g + h
+
+print(f'type of h --->',{type(h)},'\nAns.--->',{i})
+
+print()
+
