42220f1b68f90cacacda688d8f949cfead103b63
vishnukulkarni24 <vishnukulkarni2426@gmail.com>
2025-10-31 13:47:31 +0530

    second datatype

---
+++ 3_type_casting.py
@@ -0,0 +1,30 @@
+#1
+"""
+int_num = 123
+f_num = 1.23
+
+new_number = int_num + f_num
+
+print("Value:", new_number)
+print("Data Type:", type(new_number))
+
+"""
+#2.
+#num_str = "15"
+#num_int = int(num_str) 
+#print("Converted Integer:", num_int) 
+
+#3.
+#num_1 = 15
+#num_float = float(num_1)
+#print('converted type is', num_float)
+
+#4.
+#num_1 = 15.4
+#num_float = int(num_1)
+#print('converted type is', num_float)
+
+num_1 = '15.4'
+num_f = float(num_1).int()
+
+print('converted type is', num_f)
